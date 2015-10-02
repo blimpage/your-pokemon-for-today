bbf5b22c06bbe78c9d455a2f26c18efc1487e953
Pat McKenna <pat@mckenna.rocks>
2015-10-02 11:41:47 +1000

    Populate the page with Pokemon using JS

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,3 +1,13 @@
-$(function(){
-  $('body').append('woop');  
+$(function() {
+
+  var $pokemon_container = $('.js-pokemon-container'),
+      cell_classes = 'pokemon col-xs-4 col-sm-3 col-md-2 col-lg-1'
+
+  for ( i = 1; i <= 151; i++ ) {
+    $pokemon_container.append(
+      "<div class='" + cell_classes + "'>\
+        <img src='images/sugimori/" + i + ".png'>\
+      </div>");
+  }
+
 });
