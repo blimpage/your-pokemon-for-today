095536ab0da61f958be5223dae656878272c2790
Pat McKenna <pat@mckenna.rocks>
2015-10-24 16:43:26 +1100

    Add GraphicsMagick and use it to generate thumbnails!!! Whoa yeah!!!

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,63 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util');
+
+var del = require('del'); // For deletin' files
+var gutil = require('gulp-util'); // For error logging
+
+// For scripts
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var del = require('del');
+
+// For images
+var gm = require('gulp-gm'); // GraphicsMagick
+var imagemin = require('gulp-imagemin');
 
 var paths = {
   scripts: ['js/vendor/*.js', 'js/*.js'],
-  images: 'images/**/*'
+  kc_images: 'images/kc/*.png',
+  sugimori_images: 'images/sugimori/*.jpg'
 };
 
+
 gulp.task('clean', function() {
-  // You can use multiple globbing patterns as you would with `gulp.src`
+  // Delete dat build directory
   return del(['build']);
 });
 
 gulp.task('scripts', ['clean'], function() {
-  // Minify and copy all JavaScript (except vendor scripts)
+  // Minify and copy all JavaScript
   return gulp.src(paths.scripts)
       .pipe(uglify().on('error', gutil.log))
       .pipe(concat('scripts.js'))
     .pipe(gulp.dest('build/js'));
 });
 
+gulp.task('generate_thumbs', ['clean'], function() {
+  // Generate thumbs for all KC images
+  return gulp.src(paths.kc_images)
+    .pipe(gm(function(gmfile) {
+      return gmfile
+        .fuzz(5)
+        .trim()
+        .resize(160, 160)
+        .gravity('Center')
+        .extent(200, 200)
+    }))
+    .pipe(imagemin({optimizationLevel: 4}))
+    .pipe(gulp.dest('build/images/kc/thumbs'));
+});
+
+gulp.task('optimize_images', ['clean'], function(){
+  // Optimize and copy all images
+  return gulp.src([paths.kc_images, paths.sugimori_images])
+    .pipe(imagemin({optimizationLevel: 4}))
+    .pipe(gulp.dest('build/images'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
-  // gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.kc_images, ['generate_thumbs']);
+  gulp.watch([paths.kc_images, paths.sugimori_images], ['optimize_images'])
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('default', ['watch', 'scripts', 'generate_thumbs', 'optimize_images']);
