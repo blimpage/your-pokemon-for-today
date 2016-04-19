e9fd83b8fb0a27f8a9cbd82d66f84f30b2969682
Pat McKenna <pat@mckenna.rocks>
2016-04-19 21:10:08 +1000

    Progressive enhancement FTW. Render the page as a list of links initially, and then use JS to transform the links into images. If JS fails for some reason, then the page is still perfectly usable.

--- js/pokemon.js
+++ js/pokemon.js
@@ -3,6 +3,7 @@ var kc_pokemon = {
   config: {
     cell_class:        'pokemon',
     cell_hidden_class: 'hidden',
+    cell_done_class:   'js-done',
     batch_load_size:   30,
     thumbnail_size:    200
   },
@@ -10,19 +11,22 @@ var kc_pokemon = {
   $pokemon_container: $('.js-pokemon-container'),
   $loading:           $('.js-loading'),
   $window:            $(window),
+  $body:              $('body'),
 
   init: function() {
     var self = this;
 
+    self.$body.addClass('js-initialised');
+
     self._adjust_layout_for_ios();
-    self._unhide_next_batch_if_needed({retry_on_success: true});
+    self._transform_next_batch_if_needed({ retry_on_success: true });
     self._init_vendor();
 
-    self.$window.bind('scroll.pokemon', $.proxy(_.throttle(self._unhide_next_batch_if_needed, 500), self));
+    self.$window.bind('scroll.pokemon', $.proxy(_.throttle(self._transform_next_batch_if_needed, 500), self));
   },
 
-  _unhide_next_batch_if_needed: function(options) {
-    var settings = Object.assign({retry_on_success: false}, options);
+  _transform_next_batch_if_needed: function(options) {
+    var settings = Object.assign({ retry_on_success: false }, options);
 
     var window_bottom = this.$window.scrollTop() + this.$window.innerHeight(),
         loading_top   = this.$loading.position().top,
@@ -31,12 +35,64 @@ var kc_pokemon = {
     var time_to_load_more = (window_bottom + diff >= loading_top);
 
     if (time_to_load_more) {
-      this._unhide_next_batch();
+      this._transform_next_batch();
 
       if (settings.retry_on_success) {
-        this._unhide_next_batch_if_needed({retry_on_success: true});
+        this._transform_next_batch_if_needed({ retry_on_success: true });
       }
     }
+  },
+
+  _transform_next_batch: function() {
+    var self = this;
+
+    var selector = '.' + self.config.cell_class + ':not(.' + self.config.cell_done_class + ')';
+    var cells = $(selector).slice(0, self.config.batch_load_size);
+
+    if ( cells.length > 0 ) {
+      cells.each(function(index) {
+        self._transform_cell($(this));
+      });
+
+    } else {
+      self._remove_loading_spinner();
+      self.$window.unbind('scroll.pokemon');
+    }
+  },
+
+  _transform_cell: function($cell) {
+    if ( $cell.hasClass('pokemon--kc') ) {
+      this._transform_kc_cell($cell);
+    } else if ( $cell.hasClass('pokemon--sugimori') ) {
+      this._transform_sugimori_cell($cell);
+    }
+  },
+
+  _transform_kc_cell: function($cell) {
+    var $link = $cell.find('a');
+    var thumb_src = $link.attr('data-thumb');
+
+    $link.html('<img src="' + thumb_src + '">');
+
+    $cell.addClass(this.config.cell_done_class);
+  },
+
+  _transform_sugimori_cell: function($cell) {
+    var sprite_url = $cell.attr('data-sprite-url');
+    var y_offset   = $cell.attr('data-y-offset');
+
+    $inner_div = $("<div></div>").css({
+      'background-image':    'url(' + sprite_url + ')',
+      'background-position': '0 ' + y_offset
+    });
+
+    $cell.append($inner_div);
+
+    $cell.addClass(this.config.cell_done_class);
+  },
+
+  _init_vendor: function() {
+    $('.swipebox').swipebox();
   },
 
   _remove_loading_spinner: function() {
@@ -46,40 +102,10 @@ var kc_pokemon = {
   _adjust_layout_for_ios: function() {
     // User agent sniffing sucks, but parts of flexbox are still broken in mobile Safari as of iOS 9. :(
     if ( /iPad|iPhone|iPod/.test(navigator.platform) ) {
-      $('body').addClass('no-flexbox');
+      this.$body.addClass('no-flexbox');
     } else {
-      $('body').addClass('flexbox');
+      this.$body.addClass('flexbox');
     }
-  },
-
-  _unhide_next_batch: function() {
-    var selector = '.' + this.config.cell_class + '.' + this.config.cell_hidden_class;
-    var cells = $(selector).slice(0, this.config.batch_load_size);
-
-    if ( cells.length > 0 ) {
-      cells.each(function(index) {
-        var cell = $(this);
-
-        if ( cell.hasClass('pokemon--kc') ) {
-          var image = cell.find('img').first();
-          var image_src = image.attr('data-src');
-
-          image.attr('src', image_src);
-          cell.removeClass('hidden');
-
-        } else if ( cell.hasClass('pokemon--sugimori') ) {
-          cell.removeClass('hidden');
-        }
-      });
-
-    } else {
-      this._remove_loading_spinner();
-      this.$window.unbind('scroll.pokemon');
-    }
-  },
-
-  _init_vendor: function() {
-    $('.swipebox').swipebox();
   }
 };
 
