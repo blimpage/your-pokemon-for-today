7de48ed320590b1439664960e0b0545fc0f839c3
Pat McKenna <pat@mckenna.rocks>
2017-06-29 18:27:46 +1000

    Abandon PokeAPI, use rando CSV data dump instead
    PokeAPI doesn't have gen 7 Pokemon! That means it is of no use to us,
    unfortunately. I've found a Google Sheet[1] that someone made instead,
    which is up-to-date, and extracted just the data we need from it.
    
    Next, gotta loop through it and update our data with its good stuff!
    
    [1](https://docs.google.com/spreadsheets/d/1MGcNBb8irm98M9_q2Jxg_NG9nY7rB0FG4CXeTK8g2H0/edit#gid=0)

--- temp.js
+++ temp.js
@@ -1,22 +1,6 @@
-var fetch = require("node-fetch");
+var csv = require("csv");
+var fs = require("fs");
 
-for (i = 1; i < 10; i++) {
-  fetch(`http://pokeapi.co/api/v2/pokemon/${i}`)
-    .then(function(res) {
-      return res.text();
-    }).then(function(body) {
-      data = JSON.parse(body);
-      poke = {
-        id: data.id,
-        order: data.id,
-        name: data.name,
-        type1: data.types.filter(type => type.slot == 1)[0].type.name
-      };
-
-      if (data.types.length > 1) {
-        poke.type2 = data.types.filter(type => type.slot == 2)[0].type.name
-      }
-
-      console.log(poke);
-    });
-}
+csv.parse(fs.readFileSync("data/temp_pokemon_types.csv"), function(err, data) {
+  console.log(data);
+});
