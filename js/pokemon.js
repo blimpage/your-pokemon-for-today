9dabafc365e639dd2b0bcf3ab1951df096d34599
Pat McKenna <pat@mckenna.rocks>
2017-08-25 19:16:19 +1000

    Remove unneeded JavaScript
    None of this is needed anymore! We're gonna transform everything all
    at once and lazy load the images.

--- js/pokemon.js
+++ js/pokemon.js
@@ -3,12 +3,10 @@ var kc_pokemon = {
   config: {
     cell_class:       'pokemon',
     cell_done_class:  'js-done',
-    batch_load_size:  30,
     thumbnail_width:  245,
     thumbnail_height: 155
   },
 
-  loading_element: document.querySelector('.js-loading'),
   container_element: document.querySelector('.pokemon-container'),
 
   init: function() {
@@ -31,43 +29,6 @@ var kc_pokemon = {
     all_cells.forEach(function(cell) {
       self._transform_cell(cell);
     });
-  },
-
-  _transform_next_batch_if_needed: function(options) {
-    var settings = Object.assign({ retry_on_success: false }, options);
-
-    var window_bottom = window.scrollY + window.innerHeight,
-        loading_top   = this.loading_element.offsetTop,
-        diff          = this.config.thumbnail_height * 3;
-
-    var time_to_load_more = (window_bottom + diff >= loading_top);
-
-    if (time_to_load_more) {
-      this._transform_next_batch();
-
-      if (settings.retry_on_success) {
-        this._transform_next_batch_if_needed({ retry_on_success: true });
-      }
-    }
-  },
-
-  _transform_next_batch: function() {
-    var self = this;
-
-    var selector = '.' + self.config.cell_class + ':not(.' + self.config.cell_done_class + ')';
-    var all_cells_nodelist = document.querySelectorAll(selector);
-    var all_cells = Array.prototype.slice.call(all_cells_nodelist);
-    var batch = all_cells.slice(0, self.config.batch_load_size);
-
-    if ( batch.length > 0 ) {
-      batch.forEach(function(cell) {
-        self._transform_cell(cell);
-      });
-
-    } else {
-      self._remove_loading_spinner();
-      window.removeEventListener('scroll', self._throttled_transform);
-    }
   },
 
   _transform_cell: function(cell) {
@@ -141,27 +102,10 @@ var kc_pokemon = {
     cell.classList.add(this.config.cell_done_class);
   },
 
-  // _transform_sugimori_cell: function(cell) {
-  //   var sprite_url = cell.dataset.spriteUrl;
-  //   var y_offset   = cell.dataset.yOffset;
-
-  //   var inner_div = document.createElement('div');
-  //   inner_div.style.backgroundImage = 'url(' + sprite_url + ')';
-  //   inner_div.style.backgroundPosition = '0 ' + y_offset;
-
-  //   cell.appendChild(inner_div);
-
-  //   cell.classList.add(this.config.cell_done_class);
-  // },
-
   _init_vendor: function() {
     lightGallery(this.container_element, {
       selector: '.pokemon--kc'
     });
-  },
-
-  _remove_loading_spinner: function() {
-    this.loading_element.parentNode.removeChild(this.loading_element);
   },
 
   _adjust_layout_for_ios: function() {
